c01f23c682e510d9ebb2d15b28e430984f47b7e8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-03-05 18:01:01 +0200

    filters setup

--- src/components/TodoFilters.tsx
+++ src/components/TodoFilters.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoFilters = () => {
+  const [status, setStatus] = useState('all');
+  console.log('status ===', status);
+
   return (
-    <div>
-      <h3 className='text-sm'>TodoFilters</h3>
+    <div className='mt-2'>
+      <h3 className='text-sm font-medium '>Todo Filters</h3>
+      {/* add checked uncheced todos checkboxes */}
+      <div className='flex gap-2 items-center border border-slate-300 px-2'>
+        <h4>Status:</h4>
+        <label className='flex gap-1 items-center'>
+          <input
+            checked={status === 'all'}
+            onChange={(e) => setStatus(e.target.value)}
+            className='accent-slate-800'
+            type='radio'
+            name='status'
+            value={'all'}
+          />
+          <span>All</span>
+        </label>
+        <label className='flex gap-1 items-center'>
+          <input
+            checked={status === 'done'}
+            onChange={(e) => setStatus(e.target.value)}
+            className='accent-green-600'
+            type='radio'
+            name='status'
+            value={'done'}
+          />
+          <span>Done</span>
+        </label>
+        <label className='flex gap-1 items-center'>
+          <input
+            checked={status === 'notDone'}
+            onChange={(e) => setStatus(e.target.value)}
+            className='accent-red-600'
+            type='radio'
+            name='status'
+            value={'notDone'}
+          />
+          <span>Incomplete</span>
+        </label>
+      </div>
     </div>
   );
 };
