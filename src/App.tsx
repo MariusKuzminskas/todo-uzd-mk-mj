94e5b8ba946887913ddd9d1c467cb41c77ba9d77
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-03-05 16:51:07 +0200

    Update container styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import TodoApp from './components/TodoApp';
 
 function App() {
   return (
-    <div className='container min-h-screen border-[1px]'>
+    <div className='container min-h-screen '>
       <h1 className='text-2xl font-medium underline mb-10 text-slate-600'>MK uzduotis</h1>
       <TodoApp />
     </div>
