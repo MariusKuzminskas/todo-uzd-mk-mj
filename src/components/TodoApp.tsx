5ecdf559e00ea3e335c3fe378bf4ca056cde504a
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-03-05 14:23:45 +0200

    add axios, custom hook, dotenv

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import Button from './UI/Button';
+import useApiData from '../hooks/useApiData';
+import { ApiResponseType } from '../types/types';
 
+const url = import.meta.env.VITE_dummy_todos_url as string;
+console.log('url ===', url);
 const TodoApp = () => {
+  const { data, setData, error, loading } = useApiData<ApiResponseType>(url);
+
+  console.log('data ===', data);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='border border-slate-400 rounded-md px-5 py-3 w-[500px] mx-auto min-h-96'>
       <h1 className='text-2xl font-semibold '>TodoApp</h1>
@@ -16,13 +28,17 @@ const TodoApp = () => {
 
       <div className='mt-5'>
         <ul>
-          <li className='flex justify-between items-center border-b border-slate-400 py-2'>
-            <p>Todo 1</p>
-            <div>
-              <Button>Edit</Button>
-              <button className='bg-slate-500 text-white px-3 py-1 rounded-md'>Delete</button>
-            </div>
-          </li>
+          {data?.todos.map((tItem) => (
+            <li
+              key={tItem.id}
+              className='flex justify-between items-center border-b border-slate-400 py-2'>
+              <p>{tItem.todo}</p>
+              <div>
+                <Button>Edit</Button>
+                <Button className='bg-red-400'>Delete</Button>
+              </div>
+            </li>
+          ))}
           <li className='flex justify-between items-center border-b border-slate-400 py-2'>
             <p>Todo 2</p>
             <div>
